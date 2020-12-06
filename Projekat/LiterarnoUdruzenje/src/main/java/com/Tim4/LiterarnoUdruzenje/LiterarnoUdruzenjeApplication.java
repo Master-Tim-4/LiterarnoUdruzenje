4d0fd745147402214e94517352e558bb7d95ca5e
minamaras97 <minamaras@outlook.com>
2020-12-06 16:44:24 +0100

    initial commit

---
+++ Projekat/LiterarnoUdruzenje/src/main/java/com/Tim4/LiterarnoUdruzenje/LiterarnoUdruzenjeApplication.java
@@ -0,0 +1,13 @@
+package com.Tim4.LiterarnoUdruzenje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterarnoUdruzenjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterarnoUdruzenjeApplication.class, args);
+	}
+
+}
---
+++ Projekat/LiterarnoUdruzenje/src/test/java/com/Tim4/LiterarnoUdruzenje/LiterarnoUdruzenjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tim4.LiterarnoUdruzenje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterarnoUdruzenjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
